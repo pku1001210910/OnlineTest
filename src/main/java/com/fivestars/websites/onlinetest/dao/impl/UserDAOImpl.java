bcfb68cbdf2476b777fd6dd730330e18b573053c
Gao <gaoshang.pku@gmail.com>
2016-01-16 16:18:44 +0800

    Fix a problem in configuration file

--- src/main/java/com/fivestars/websites/onlinetest/dao/impl/UserDAOImpl.java
+++ src/main/java/com/fivestars/websites/onlinetest/dao/impl/UserDAOImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import com.fivestars.websites.onlinetest.dao.UserDAO;
 import com.fivestars.websites.onlinetest.model.User;
 
-@Repository("UserDao")
+@Repository("userDao")
 public class UserDAOImpl extends GenericDAOImpl<User, String> implements UserDAO {
 
 }
