52698b44bcf0399d9c7448790adce421d7ebc303
Gao <gaoshang.pku@gmail.com>
2016-01-16 13:10:49 +0800

    Add some costs

---
+++ src/main/java/com/fivestars/websites/onlinetest/constant/QuizConst.java
@@ -0,0 +1,14 @@
+package com.fivestars.websites.onlinetest.constant;
+
+public class QuizConst {
+
+	// quiz subject type 
+	public static final int TYPE_SINGLE_CHOICE = 0;
+	public static final int TYPE_MULTIPLE_CHOICE = 1;
+	public static final int TYPE_COMPLETION = 3;
+	public static final int TYPE_ANSWER = 4;
+	
+	// quiz status
+	public static final int STATUS_TEMPORARY_SAVE = 0;
+	public static final int STATUS_SUBMITTED = 1;
+}
---
+++ src/main/java/com/fivestars/websites/onlinetest/constant/UserConst.java
@@ -0,0 +1,10 @@
+package com.fivestars.websites.onlinetest.constant;
+
+public class UserConst {
+
+	// user identifier
+	public static final byte IS_NOT_ADMIN = 0;
+	public static final byte IS_ADMIN = 1;
+	public static final byte IS_NOT_TEACHER = 0;
+	public static final byte IS_TEACHER = 1;
+}
