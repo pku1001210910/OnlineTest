eb211bfeda066642921e116d812fca21f481ef25
liang_w <liangwenbo_lm@163.com>
2016-01-19 23:35:36 +0800

    modify the session.error to request.error for login user error

--- src/main/java/com/fivestars/websites/onlinetest/action/HomeAction.java
+++ src/main/java/com/fivestars/websites/onlinetest/action/HomeAction.java
@@ -1,10 +1,14 @@
 package com.fivestars.websites.onlinetest.action;
 
+import java.util.Map;
+
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 
+import com.fivestars.websites.onlinetest.model.User;
 import com.opensymphony.xwork2.ActionSupport;
 
 @ParentPackage("user")
@@ -14,6 +18,13 @@ public class HomeAction {
 
 	@Action("home")
 	public String home() {
+		Map session = ServletActionContext.getContext().getSession();
+		String error = (String) session.get("error");
+		if(error != null && !"".equals(error)) {
+			session.remove("error");
+			Map request = (Map) ServletActionContext.getContext().get("request");
+			request.put("error", error);
+		}
 		return ActionSupport.SUCCESS;
 	}
 }
