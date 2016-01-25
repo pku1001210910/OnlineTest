c1f211ffc9e4e9ce535a91927440dc93b120d550
liang_w <liangwenbo_lm@163.com>
2016-01-25 13:08:45 +0800

    add the quiz list page

--- src/main/java/com/fivestars/websites/onlinetest/model/SubjectItem.java
+++ src/main/java/com/fivestars/websites/onlinetest/model/SubjectItem.java
@@ -5,6 +5,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
